df0f8a833e73e31b5391ad073eb09981b6b4a035
Amruth <ammumandappa@gmail.com>
2024-07-05 15:34:53 +0530

    .

--- 01portfolio/src/data/constants.jsx
+++ 01portfolio/src/data/constants.jsx
@@ -1,20 +1,20 @@
 export const Bio = {
-    name: "Rishav Chanda",
+    name: "Amruth Mandappa T S",
     roles: [
       "Full Stack Developer",
       "Android Developer",
-      "UI/UX Designer",
+      "IOS  Developer",
       "Programmer",
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/rishavchanda",
-    resume:
-      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-    linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-    twitter: "https://twitter.com/RishavChanda",
-    insta: "https://www.instagram.com/rishav_chanda/",
-    facebook: "https://www.facebook.com/rishav.chanda.165/",
+    github: "https://github.com/Amruth56",
+    resume:"https://drive.google.com/file/d/1o1zoXJ6YnGAR03WjFMWvNK-xTa_HX2j1/view?usp=sharing",
+
+    linkedin: "https://www.linkedin.com/in/amruth-mandappa-t-s-48838a203/",
+    twitter: "https://x.com/Amruth_Mandappa",
+    insta: "https://www.instagram.com/myself.amruth/",
+    
   };
   
   export const skills = [
