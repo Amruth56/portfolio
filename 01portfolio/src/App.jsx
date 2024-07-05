0bd3eea7d9878ddc9f8af49af1d17ccd20ea0995
Amruth <ammumandappa@gmail.com>
2024-07-05 17:44:53 +0530

    .

--- 01portfolio/src/App.jsx
+++ 01portfolio/src/App.jsx
@@ -1,12 +1,28 @@
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import Hero from "./components/HeroSection";
+import Skills from "./components/Skills";
 
-import './App.css'
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
 
 function App() {
-
   return (
     <>
-    Amruth</>
-  )
+      <ThemeProvider theme={darkTheme}>
+        <Navbar></Navbar>
+        <Body>
+          <Hero></Hero>
+          <Skills></Skills>
+        </Body>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
