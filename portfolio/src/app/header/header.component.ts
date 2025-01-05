da41a534cbed0e5d3de5953b8c2b403a56bc552d
Amruth <ammumandappa@gmail.com>
2025-01-05 21:16:31 +0530

    intro page done

--- portfolio/src/app/header/header.component.ts
+++ portfolio/src/app/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { SkillsComponent } from '../skills/skills.component';
 import { InternshipsComponent } from '../internships/internships.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { AcheivementsComponent } from "../acheivements/acheivements.component";
 import { ContactMeComponent } from "../contact-me/contact-me.component";
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
+import { ButtonModule } from 'primeng/button'; 
+import { DialogModule } from 'primeng/dialog'; 
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-header',
   standalone:true,
-  imports: [CommonModule, SkillsComponent, InternshipsComponent, ProjectsComponent, AcheivementsComponent, ContactMeComponent],
+  imports: [CommonModule, 
+    SkillsComponent, 
+    InternshipsComponent, 
+    ProjectsComponent, 
+    AcheivementsComponent, 
+    ContactMeComponent, 
+    HomeComponent,
+    ButtonModule,
+     DialogModule,
+     TooltipModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
